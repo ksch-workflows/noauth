8cd57ef8cc94eebf1ab05d5fdaf57a1d2af318b8
Jan Mewes <5235584+jmewes@users.noreply.github.com>
2022-04-16 17:13:58 +0200

    Add support for CORS

--- bin/server.dart
+++ bin/server.dart
@@ -4,6 +4,7 @@ import 'package:noauth/auth_service.dart';
 import 'package:noauth/token_service.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
+import 'package:shelf_cors_headers/shelf_cors_headers.dart';
 
 const int defaultPort = 8080;
 
@@ -11,16 +12,23 @@ var _authService = AuthService();
 var _tokenService = TokenService();
 
 void main(List<String> args) async {
-  var _handler = Pipeline().addMiddleware(logRequests()).addHandler(
-        Cascade()
-            .add(
-              _authService.router,
-            )
-            .add(
-              _tokenService.router,
-            )
-            .handler,
-      );
+  var _handler = Pipeline()
+      .addMiddleware(
+        logRequests(),
+      )
+      .addMiddleware(corsHeaders(
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+      ))
+      .addHandler(Cascade()
+          .add(
+            _authService.router,
+          )
+          .add(
+            _tokenService.router,
+          )
+          .handler);
   var server = await serve(_handler, InternetAddress.anyIPv4, args.port);
   print('Server listening on port ${server.port}');
 }
